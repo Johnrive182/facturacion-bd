0b9cf5bea5b2035956b041c513cd9ca0b77e2cf8
COMPUMAS <COMPUMAS@DESKTOP-8FQN5BR>
2021-05-31 23:26:16 -0500

    Cambios pedidos

--- factura/src/main/java/edu/ucacue/factura/controlador/Principal.java
+++ factura/src/main/java/edu/ucacue/factura/controlador/Principal.java
@@ -33,49 +33,94 @@ public class Principal {
 			entradaTeclado = entradaEscaner.nextInt();
 			switch (entradaTeclado) {
 			case 1:
-				//Gererar el objeto Persona pidiendo los datos por teclado
-				System.out.println("Ingrese el nombre: ");
-				String nombre= entradaEscaner.next();
-				System.out.println("Ingrese el apellido: ");
-				String apellido= entradaEscaner.next();
-				System.out.println("Ingrese la cédula: ");
-				String cedula= entradaEscaner.next();
-				System.out.println("Ingrese el teléfono: ");
-				String telefono= entradaEscaner.next();
-				
-				Persona p = new Persona( nombre, apellido, telefono, cedula);
-				personaRepository.save(p);
-				
+				try {
+					// Gererar el objeto Persona pidiendo los datos por teclado
+					System.out.println("Ingrese el nombre: ");
+					String nombre = entradaEscaner.next();
+					System.out.println("Ingrese el apellido: ");
+					String apellido = entradaEscaner.next();
+					System.out.println("Ingrese el teléfono: ");
+					String telefono = entradaEscaner.next();
+					System.out.println("Ingrese la cédula: ");
+					String cedula = entradaEscaner.next();
+
+					int a, s = 0, ac, r = 0, ult;
+					for (int i = 0; i < cedula.length() - 1; i++) {
+						a = Integer.parseInt(cedula.charAt(i) + "");
+						if (i % 2 == 0) {
+							a = a * 2;
+							if (a > 9) {
+								a = a - 9;
+							}
+						}
+						s = s + a;
+						if (s % 10 != 0) {
+							ac = ((s / 10) + 1) * 10;
+							r = ac - s;
+						}
+						ult = Integer.parseInt(cedula.charAt(9) + "");
+
+						if (ult == r) {
+							System.out.println("La cedula ingresada es correcta");
+						}
+					}
+
+					Persona p = new Persona(nombre, apellido, telefono, cedula);
+					personaRepository.save(p);
+				} catch (Exception e) {
+					System.out.println("La celula ingresada es incorrecta");
+				}
 				break;
 			case 2:
-				//1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
-				//2 ENcontrar a la persona por el número de la cédula
-				//3 modificar y cambiar los datos
+				try {
+					// 1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
+					System.out.println("Ingrese la cédula que quiera modificar: ");
+					String c1 = entradaEscaner.next();
+					// 2 ENcontrar a la persona por el número de la cédula
+					Persona personamodificada = personaRepository.findByCedula(c1);
+					// 3 modificar y cambiar los datos
+					System.out.println("Ingrese la cédula nueva: ");
+					String cac = entradaEscaner.next();
+					personamodificada.setCedula(cac);
+					personaRepository.save(personamodificada);
+				} catch (Exception e) {
+					System.out.println("La celula ingresada no existe");
+				}
 				break;
 			case 3:
 				try {
-				System.out.println("Ingrese el número de cédula que desea eliminar: ");
-				String cedulaBuscar= entradaEscaner.next();
-				Persona personaRecuperada= personaRepository.findByCedula(cedulaBuscar);
-				personaRepository.delete(personaRecuperada);
-				
-			    //1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
-				//2 ENcontrar a la persona por el número de la cédula
-				//3 Eliminar
-				
-				}catch(Exception e) {System.out.println("Existe un problema con la cédula ingresada ");}
+					System.out.println("Ingrese el nombre que quiera eliminar: ");
+					String nomb = entradaEscaner.next();
+					List<Persona> personanom = personaRepository.findByNombre(nomb);
+					System.out.println("Id|" + "\t" + "|Cedula|" +"\t" + "|Nombre|" + "\t" + "|Apellido|");
+					for (Persona persona : personanom) {
+						System.out.println(persona.getId()+ " " +persona.getCedula() + " " + persona.getNombre() + " " + persona.getApellido());
+					}
+					System.out.println("Ingrese el id que quiera eliminar: ");
+					int id = entradaEscaner.nextInt();
+					Persona personaid = personaRepository.findById(id);
+					personaRepository.delete(personaid);
+
+					// 1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
+					// 2 ENcontrar a la persona por el número de la cédula
+					// 3 Eliminar
+
+				} catch (Exception e) {
+					System.out.println("Existe un problema con la cédula ingresada ");
+				}
 				break;
 			case 4:
-					//Buscar todas las personas que están en la BD e imprimir os resultados
-				
-				List<Persona> personas= personaRepository.findAll();
-				System.out.println("Cédula|"+ "\t"+ "|Nombre|"+"\t" + "|Apellido|");
+				// Buscar todas las personas que están en la BD e imprimir os resultados
+				System.out.println("Ingrese el nombre que desee buscar: ");
+				String nom = entradaEscaner.next();
+				List<Persona> personas = personaRepository.findByNombre(nom);
+				System.out.println("Cédula|" + "\t" + "|Nombre|" + "\t" + "|Apellido|");
 				for (Persona persona : personas) {
-					System.out.println(persona.getCedula()+" "+ persona.getNombre()+" " + persona.getApellido());
+					System.out.println(persona.getCedula() + " " + persona.getNombre() + " " + persona.getApellido());
 				}
 				break;
 			case 5:
-				System.exit(0);	
+				System.exit(0);
 				break;
 
 			default:
--- factura/src/main/java/edu/ucacue/factura/infraestructura/repositorio/PersonaRepositorio.java
+++ factura/src/main/java/edu/ucacue/factura/infraestructura/repositorio/PersonaRepositorio.java
@@ -3,8 +3,35 @@ package edu.ucacue.factura.infraestructura.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.ucacue.factura.modelo.Persona;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonaRepositorio extends JpaRepository<Persona, Integer> {
 
+
 	Persona findByCedula(String cedula);
+
+	List<Persona> findByNombreLike(String nombre);
+
+	List<Persona> findByNombreAndApellido(String nombre, String apellido);
+
+	List<Persona> findByNombre(String nombre);
+	
+	Persona findById(int id);
+
+	// JPQL
+	@Query("select p from Persona p where p.nombre =  :nombre")
+	List<Persona> buscarPorNombres(@Param("nombre") String nombre);
+
+	@Query("select p from Persona p where p.apellido = :apellido")
+	List<Persona> buscarPorApellido(@Param("apellido") String apellido);
+
+	@Query("select p from Persona p where p.apellido = :apellido and p.nombre = :nombre")
+	List<Persona> buscarPorApellidoYnombre(@Param("apellido") String apellido, @Param("nombre") String nombre);
+	
+	@Query("select p from Persona p where p.apellido like :nombre")
+	List<Persona> buscarPorNombreLike(@Param("nombre") String nombre);
+
 }
